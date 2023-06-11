72c4e9aaff7f77f17c52206a638c16a505361e17
prasadrayudu <43725655+prasadrayudu@users.noreply.github.com>
2023-06-11 09:46:54 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! commit4 test"
+    return "Hello World! commit5 test"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
